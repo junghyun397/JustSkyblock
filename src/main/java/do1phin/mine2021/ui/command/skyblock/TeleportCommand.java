1b147490680181cba4d6df0c0b4f0ea5ffb59549
junghyun397 <junghyun397@gmail.com>
2021-02-02 22:15:57 +0900

    Implement teleport to @r feature

--- src/main/java/do1phin/mine2021/ui/command/skyblock/TeleportCommand.java
+++ src/main/java/do1phin/mine2021/ui/command/skyblock/TeleportCommand.java
@@ -8,9 +8,11 @@ import do1phin.mine2021.ServerAgent;
 import do1phin.mine2021.data.Config;
 import do1phin.mine2021.data.db.DatabaseAgent;
 import do1phin.mine2021.skyblock.SkyBlockAgent;
+import do1phin.mine2021.skyblock.data.SkyblockData;
 import do1phin.mine2021.ui.MessageAgent;
 
 import java.util.Optional;
+import java.util.Random;
 import java.util.UUID;
 
 public class TeleportCommand extends SkyblockCommand {
@@ -33,18 +35,33 @@ public class TeleportCommand extends SkyblockCommand {
     public boolean execute(CommandSender commandSender, String ignored, String[] args) {
         if (!this.checkExecutable(commandSender)) return false;
 
-        if (args.length == 0) {
+        if (args.length == 0 || args[0].equals("@s")) {
             this.skyBlockAgent.teleportPlayerToIsland((Player) commandSender, ((Player) commandSender).getUniqueId(), commandSender.getName());
+            return true;
+        }
+
+        final UUID targetUUID;
+        final String targetName;
+        if (args[0].equals("@r")) {
+            final Optional<SkyblockData> skyblockData
+                    = this.databaseAgent.getSkyblockDataBySection(new Random().nextInt(this.databaseAgent.getNextSection()));
+            if (skyblockData.isPresent()) {
+                targetUUID = skyblockData.get().getOwnerUUID();
+                targetName = skyblockData.get().getOwnerName();
+            } else return false;
         } else {
-            final Optional<UUID> targetUUID = this.databaseAgent.getUUIDByPlayerName(args[0]);
-            if (!targetUUID.isPresent()) {
+            final Optional<UUID> maybeUUID = this.databaseAgent.getUUIDByPlayerName(args[0]);
+            if (!maybeUUID.isPresent()) {
                 this.messageAgent.sendMessage(commandSender, "command.skyblock.teleport-command.teleport-failed-playernotfound",
                         new String[]{"%player"}, new String[]{args[0]});
                 return false;
             }
-
-            this.skyBlockAgent.teleportPlayerToIsland((Player) commandSender, targetUUID.get(), args[0]);
+            targetUUID = maybeUUID.get();
+            targetName = args[0];
         }
+
+        this.skyBlockAgent.teleportPlayerToIsland((Player) commandSender, targetUUID, targetName);
+
 
         return true;
     }
